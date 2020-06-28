b0b540394f53dba30cf8b4c2e62803b68d48c80c
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-06-28 16:40:13 +0100

    Uploaded helloworld.py
    A starter project, using separators, comments, string conversions and string formatting

---
+++ helloworld.py
@@ -0,0 +1,6 @@
+print("Hello world!", "I'm going to hack ciphers with Python", "Yaaaaaay!!!!!", sep="\n")
+# Hip, hip, hooray!!
+print("Which challenge are we preparing for?\n")
+ans = str(input())
+print("I'm gonna come first in the %s!!" % (ans))
+# end
