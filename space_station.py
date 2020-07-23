02e46e32e73a99f1f6f9d7207557c702b02d6f52
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-07-23 09:32:26 +0100

    Started space station project
    https://projects.raspberrypi.org/en/projects/where-is-the-space-station

---
+++ space_station.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+import json
+import turtle
+import urllib.request
+
+url = 'http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print(result)
