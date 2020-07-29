d9e729823defc4a0e6498b963366012df21c36ab
Adannaya <64081845+Adannaya@users.noreply.github.com>
2020-07-29 17:37:22 +0100

    Create spambot.py
    Open a webpage such as Instagram DMs or WhatsApp Web and run the program

---
+++ spambot.py
@@ -0,0 +1,14 @@
+# Make sure periodic.txt is in the same directory as spambot.py
+
+from os import system as s
+s("pip install pyautogui")
+
+import pyautogui, time
+
+time.sleep(5)
+spamtext = open("periodic.txt", "r")
+for line in spamtext:
+  pyautogui.typewrite(line)
+  pyautogui.press("enter")
+pyautogui.typewrite("Now you have no excuse for failing Chemistry.")
+pyautogui.press("enter")
